09bd02b29d4ad16e1ec31e37696fe947c572bb1a
Paul Fruitful <fruitful2007@outlook.com>
2022-11-06 21:42:57 +0000

    Login Layout Set

--- resources/views/user/login.blade.php
+++ resources/views/user/login.blade.php
@@ -10,14 +10,22 @@
      <div class="flex justify-center items-center flex-wrap h-full g-6 text-gray-800">
     
        <div class="md:w-8/12 lg:w-5/12 lg:ml-20">
-      <h1 class="text-center text-4xl font-bold mb-12">Login <span class="text-blue-700">Now</span></h1>
+      <h1 class="text-center text-4xl font-bold mb-12">Login <span class="text-green-700">Now</span></h1>
          <form action="/login" method="POST">
             @csrf
+            @if ($errors)
+            <ul class="m-2">
+                @foreach ($errors->all() as $message)
+                    <li class="text-red-700 text-md font-bold">{{$message}}</li>
+                @endforeach
+               </ul>
+            @endif
            <!-- Email input -->
            <div class="mb-6">
              <input
                type="text"
                class="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+               name="email"
                placeholder="Email address"
              />
            </div>
@@ -27,6 +35,7 @@
              <input
                type="password"
                class="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+               name="password"
                placeholder="Password"
              />
            </div>
@@ -50,7 +59,7 @@
            <!-- Submit button -->
            <button
              type="submit"
-             class="inline-block px-7 py-3 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out w-full"
+             class="inline-block px-7 py-3 bg-green-800 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out w-full"
              data-mdb-ripple="true"
              data-mdb-ripple-color="light"
            >
