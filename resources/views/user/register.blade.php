ec83f9131c8515aaefa8b77464a11cbecb045dc8
Paul Fruitful <fruitful2007@outlook.com>
2022-11-06 23:27:40 +0100

    Register view

--- resources/views/user/register.blade.php
+++ resources/views/user/register.blade.php
@@ -48,6 +48,15 @@
                placeholder="Password"
              />
            </div>
+                <!-- Password input -->
+                <div class="mb-6">
+                  <input
+                    type="password"
+                    class="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                    name="password_confirmation"
+                    placeholder="Confirm Password"
+                  />
+                </div>
  
            <div class="flex justify-between items-center mb-6">
           
