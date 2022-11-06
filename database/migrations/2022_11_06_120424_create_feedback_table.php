08b421dedbf45d0c582e8766cb758cb51e064888
Paul Fruitful <fruitful2007@outlook.com>
2022-11-06 12:09:08 +0000

    Feedback migration

--- database/migrations/2022_11_06_120424_create_feedback_table.php
+++ database/migrations/2022_11_06_120424_create_feedback_table.php
@@ -15,6 +15,12 @@ return new class extends Migration
     {
         Schema::create('feedback', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('product_id')->constrained()->onDelete('cascade');
+            $table->text('title');
+            $table->text('name');
+            $table->longText('feedback');
+            $table->integer('upvote');
+            $table->integer('downvote');
             $table->timestamps();
         });
     }
