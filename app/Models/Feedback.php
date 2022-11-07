56af86fccf41daadc3b26941f147ce52200db8a6
Paul Fruitful <fruitful2007@outlook.com>
2022-11-07 19:33:34 +0100

    Feedback model fillables

--- app/Models/Feedback.php
+++ app/Models/Feedback.php
@@ -2,13 +2,19 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Product;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Feedback extends Model
 {
     use HasFactory;
-
+    protected $fillable=[
+        'title',
+        'description',
+        'upvote',
+        'downvote'
+    ];
     public function product(){
         return $this->belongsTo(Product::class,'product_id');
     }
