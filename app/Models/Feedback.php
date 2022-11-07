b5cc82f96eb8dce912304567653d6ff22f671ac5
Paul Fruitful <fruitful2007@outlook.com>
2022-11-07 19:29:23 +0100

    Model relationships part 2

--- app/Models/Feedback.php
+++ app/Models/Feedback.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Feedback extends Model
 {
     use HasFactory;
+
+    public function product(){
+        return $this->belongsTo(Product::class,'product_id');
+    }
 }
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use App\Models\User;
+use App\Models\Feedback;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
@@ -12,5 +13,8 @@ class Product extends Model
 
     public function user(){
         return $this->belongsTo(User::class,'user_id');
+    }
+    public function feedback(){
+        return $this->hasMany(Feedback::class,'product_id');
     }
 }
