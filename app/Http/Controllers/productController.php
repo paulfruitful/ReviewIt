ea98b178e660c1ae453503ca87d6d2865570aefd
Paul Fruitful <fruitful2007@outlook.com>
2022-11-08 21:32:46 +0100

    Product Controller

--- app/Http/Controllers/productController.php
+++ app/Http/Controllers/productController.php
@@ -2,9 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class productController extends Controller
 {
-    //
+    public function create(){
+        return view('product.create');
+    }
+
+    public function edit(Product $product, Request $request){
+          if($product->id()==auth()->id()){
+
+          }else{
+            return abort('404');
+          }
+    }
 }
