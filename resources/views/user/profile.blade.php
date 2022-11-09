c3d1487f27b15ad35114f35943fdd149ac166be4
Paul Fruitful <fruitful2007@outlook.com>
2022-11-09 19:51:16 +0100

    User Profile View

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -161,7 +161,34 @@
 				class="flex flex-row justify-between border-b
 				dark:border-gray-600 dark:text-gray-400 transition duration-500
 				ease-in-out">
-				
+				<div class="flex">
+					<!-- Top NavBar -->
+
+					<a
+						href="users-dashboard/"
+						class="py-2 block text-green-500 border-green-500
+						dark:text-green-200 dark:border-green-200
+						focus:outline-none border-b-2 font-medium capitalize
+						transition duration-500 ease-in-out">
+						users
+					</a>
+					<button
+						class="ml-6 py-2 block border-b-2 border-transparent
+						focus:outline-none font-medium capitalize text-center
+						focus:text-green-500 focus:border-green-500
+						dark-focus:text-green-200 dark-focus:border-green-200
+						transition duration-500 ease-in-out">
+						role
+					</button>
+					<button
+						class="ml-6 py-2 block border-b-2 border-transparent
+						focus:outline-none font-medium capitalize text-center
+						focus:text-green-500 focus:border-green-500
+						dark-focus:text-green-200 dark-focus:border-green-200
+						transition duration-500 ease-in-out">
+						access rights
+					</button>
+				</div>
 
 				<div class="flex items-center select-none">
 					<span
