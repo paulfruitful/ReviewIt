b68f6ffff3bd43349a018909149440906ef03f88
Paul Fruitful <fruitful2007@outlook.com>
2022-11-11 20:34:29 +0100

    Create Product Review view

--- resources/views/product/create.blade.php
+++ resources/views/product/create.blade.php
@@ -4,7 +4,13 @@
 
 
 
-
+<!doctype html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <script src="https://cdn.tailwindcss.com"></script>
+</head>
 
 <div class="heading text-center font-bold text-2xl m-5 text-gray-800">New Post</div>
 <style>
@@ -12,7 +18,7 @@
 </style>
   <div class="editor mx-auto w-10/12 flex flex-col text-gray-800 border border-gray-300 p-4 shadow-lg max-w-2xl">
     <input class="title bg-gray-100 border border-gray-300 p-2 mb-4 outline-none" spellcheck="false" placeholder="Title" type="text">
-    <textarea class="description bg-gray-100 sec p-3 h-60 border border-gray-300 outline-none" spellcheck="false" placeholder="Describe everything about this post here"></textarea>
+    <textarea class="description bg-gray-100 sec p-3 h-60 border border-gray-300 outline-none" spellcheck="false" placeholder="Describe everything about this post here" name="description"></textarea>
     
     <!-- icons -->
     <div class="icons flex text-gray-500 m-2">
@@ -27,3 +33,8 @@
       <div class="btn border border-indigo-500 p-1 px-4 font-semibold cursor-pointer text-gray-200 ml-2 bg-indigo-500">Post</div>
     </div>
   </div>
+  <script src="https://cdn.ckeditor.com/4.16.0/standard/ckeditor.js"></script>
+
+    <script>
+        CKEDITOR.replace( 'description' );
+    </script>
