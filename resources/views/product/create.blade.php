76e78a1f8e06a15fbeb741f5c53d27219f279999
Paul Fruitful <fruitful2007@outlook.com>
2022-11-11 20:37:25 +0100

    Create Product Review view

--- resources/views/product/create.blade.php
+++ resources/views/product/create.blade.php
@@ -11,14 +11,15 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <script src="https://cdn.tailwindcss.com"></script>
 </head>
-
-<div class="heading text-center font-bold text-2xl m-5 text-gray-800">New Post</div>
+<form action="/product/create" method="post">
+    @csrf
+<div class="heading text-center font-bold text-2xl m-5 text-gray-800">New Review</div>
 <style>
   body {background:white !important;}
 </style>
   <div class="editor mx-auto w-10/12 flex flex-col text-gray-800 border border-gray-300 p-4 shadow-lg max-w-2xl">
-    <input class="title bg-gray-100 border border-gray-300 p-2 mb-4 outline-none" spellcheck="false" placeholder="Title" type="text">
-    <textarea class="description bg-gray-100 sec p-3 h-60 border border-gray-300 outline-none" spellcheck="false" placeholder="Describe everything about this post here" name="description"></textarea>
+    <input class="title bg-gray-100 border border-gray-300 p-2 mb-4 outline-none" spellcheck="true" name="title" placeholder="Title" type="text">
+    <textarea class="description bg-gray-100 sec p-3 h-60 border border-gray-300 outline-none" spellcheck="true" placeholder="Describe everything about this post here" name="description"></textarea>
     
     <!-- icons -->
     <div class="icons flex text-gray-500 m-2">
@@ -38,3 +39,4 @@
     <script>
         CKEDITOR.replace( 'description' );
     </script>
+    </form>
