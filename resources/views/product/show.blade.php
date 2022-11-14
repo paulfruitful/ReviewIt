e567f0e672b75066493f051156242841c52010de
Paul Fruitful <fruitful2007@outlook.com>
2022-11-14 17:25:34 +0100

    Reviewer Can View Feedbacks

--- resources/views/product/show.blade.php
+++ resources/views/product/show.blade.php
@@ -44,6 +44,40 @@
   
         
         </div>
+        @if (auth()->id()==$product->user_id)
+  <p class="text-center text-4xl font-bold my-8 text-gray-700">Feedbacks <span class="bg-green-600 rounded-full shadow-xl py-3 px-3 text-white">{{count($product->feedback)}}</span></p>
+     
+ @if(count($product->feedback)>0)
+                      @foreach ($product->feedback as $feedback)   
+                      <div class="flex justify-center relative my-12 top-1/3">
+
+
+            <div class="relative w-full  grid grid-cols-1 gap-4 p-4  border rounded-lg bg-white shadow-lg">
+                <div class="relative flex gap-4">
+                    <img src="https://icons.iconarchive.com/icons/diversity-avatars/avatars/256/charlie-chaplin-icon.png" class="relative rounded-lg -top-8 -mb-4 bg-white border h-20 w-20" alt="" loading="lazy">
+                    <div class="flex flex-col w-full">
+
+                     
+                          <div class="flex flex-row justify-between">
+                            <p class="relative text-xl whitespace-nowrap truncate overflow-hidden">{{$feedback->name}}</p>
+                            <a class="text-gray-500 text-xl" href="#"><i class="fa-solid fa-trash"></i></a>
+                        </div>
+                      
+                        
+                    
+                        <p class="text-gray-400 text-sm">{{$feedback->created_at->format('jS F Y h:i:s A')}}</p>
+                    </div>
+                </div>
+                <p class="-mt-4 text-gray-500">{{$feedback->feedback}}</p>
+            </div>
+               
+            
+            
+            </div>  @endforeach
+                        @else
+                        @endif
+            @else
+       
       <!--  <p class="text-center text-4xl font-bold my-8 text-gray-700">Feedbacks <span class="bg-green-600 rounded-full shadow-xl py-3 px-3 text-white">{{count($product->feedback)}}</span></p>
         <div class="flex justify-center relative my-12 top-1/3">
 
@@ -67,7 +101,11 @@
             
             
             
-            </div> -->
+            </div> --> @endif
+            
+        @if (auth()->id()==$product->user_id)
+        
+        @else
 <p class="text-center text-4xl font-bold my-8 text-gray-700">Drop A Feedback</p>
 <!-- comment form -->
 <div class="flex mx-auto items-center justify-center shadow-lg mt-8 w-full mb-4 ">
@@ -100,6 +138,7 @@
        </form>
     </div>
  </div>
+ @endif
  <!--
 <div class="flex items-center justify-center p-12">
     <!-- Author: FormBold Team -->
